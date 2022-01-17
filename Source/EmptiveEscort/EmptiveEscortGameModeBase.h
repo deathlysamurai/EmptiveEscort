2356b16b4ac7f396c854ac941b07cba39e1601b5
Mathew Humphrey <deathlysamurai@live.com>
2022-01-17 11:28:00 -0600

    Initial commit

---
+++ Source/EmptiveEscort/EmptiveEscortGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EmptiveEscortGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPTIVEESCORT_API AEmptiveEscortGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
